197e3ddce0f5ba1370f39fefb517e06fe68aeede
sharif <sharif.jatri@gmail.com>
2022-08-01 17:15:52 +0600

    product list on homepage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,24 +8,12 @@
 </template>
 
 <script>
-import items from "@/data/items";
-
 export default {
   name: 'HelloWorld',
-  data() {
-    return {
-      items: items
-    }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  .product-cards-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-
-  }
+  
 </style>
