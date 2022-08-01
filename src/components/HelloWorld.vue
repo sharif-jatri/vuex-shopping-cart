9eb8067799f7dcfea6cbc21bbec7b99acf5b0972
sharif <sharif.jatri@gmail.com>
2022-08-01 16:08:46 +0600

    product dummy data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="hello">
     <h1>This is an home page</h1>
+    <div class="product-cards-container">
+
+    </div>
   </div>
 </template>
 
 <script>
+import items from "@/data/items";
+
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      items: items
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .product-cards-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+  }
 </style>
