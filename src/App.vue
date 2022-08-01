df6fdf0db1da019b7b216593e4fbeb73ea618b63
sharif <sharif.jatri@gmail.com>
2022-08-01 11:37:16 +0600

    home and cart component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :class="{active: $route.name === 'home'}" :to="{ name: 'home'}">Home</router-link> |
+    <router-link :class="{active: $route.name === 'cart'}" :to="{ name: 'cart'}">Cart</router-link>
   </nav>
   <router-view/>
 </template>
